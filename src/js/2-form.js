f0cd44eb09d4c63866e6bd0f8ad5b61f6ad99508
Andrii-Didukh <andriididukh@gmail.com>
2025-03-23 11:50:25 +0200

    completed homework 1

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,39 +1,39 @@
 let formData = {
-    email: "",
-    message: "",
+  email: '',
+  message: '',
 };
 
-const form = document.querySelector(".feedback-form");
+const form = document.querySelector('.feedback-form');
 
-form.addEventListener("input", (e) => {
-    const fieldName = e.target.name;
-    const fieldValue = e.target.value.trim();
+form.addEventListener('input', e => {
+  const fieldName = e.target.name;
+  const fieldValue = e.target.value.trim();
 
-    if (!fieldValue) {
-        return;
-    }
-    formData[fieldName] = fieldValue;
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+  if (!fieldValue) {
+    return;
+  }
+  formData[fieldName] = fieldValue;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener('submit', e => {
+  e.preventDefault();
 
-    console.log(formData);
-    formData = { email: "", message: "" };
-    e.currentTarget.reset();
-    localStorage.removeItem("feedback-form-state");
+  console.log(formData);
+  formData = { email: '', message: '' };
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
 });
 
 function loadFormData() {
-    const storageData = JSON.parse(localStorage.getItem("feedback-form-state"));
-    if (!storageData) {
-        return;
-    }
-    Object.keys(storageData).forEach((key) => {
-        form.elements[key].value = storageData[key];
-        formData[key] = storageData[key];
-    });
+  const storageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (!storageData) {
+    return;
+  }
+  Object.keys(storageData).forEach(key => {
+    form.elements[key].value = storageData[key];
+    formData[key] = storageData[key];
+  });
 }
 
 loadFormData();
